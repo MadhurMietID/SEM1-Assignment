c4d22c162a8cf30b085746c624dcca5d41da5631
MadhurMietID <146841949+MadhurMietID@users.noreply.github.com>
2024-01-06 11:54:24 +0530

    Up check_given_string_is_in_password_validation.c

--- check_given_string_is_in_password_validation.c
+++ check_given_string_is_in_password_validation.c
@@ -1,4 +1,7 @@
-// c program to check if the given string is in password validation
+// C program to check if a given string is a valid password.
+// The password must have a length of at least 8 characters,
+// contain at least one lowercase letter, one uppercase letter,one digit, and one special character.
+
 #include <stdio.h>
 #include <string.h>
 int main()
@@ -9,7 +12,7 @@ int main()
     scanf("%s",str);
     n = strlen(str);
     // Check if the length of the password is at least 8 characters
-    if (strlen(str) < 8)
+    if (n < 8)
     {
         printf("Invalid password\n");
     }
@@ -55,6 +58,23 @@ int main()
         printf("Invalid password\n");
         return 0;
     }
+    // Check if the password contains at least one special character
+    for (i = 0; i < n; i++)
+    {
+        if (str[i] == '!' || str[i] == '@' || str[i] == '#' || str[i] == '^' || str[i] == '&' || str[i] == '*' || str[i] == '(' || str[i] == ')' || str[i] == '-' || str[i] == '+' || str[i] == '=' || str[i] == '[' || str[i] == ']' || str[i] == '{' || str[i] == '}' || str[i] == ';' || str[i] == ':' || str[i] == ',' || str[i] == '.' || str[i] == '?' || str[i] == '/')
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf("Invalid password\n");
+        return 0;   
+    }
+    else
+    {
     printf("Valid password\n");
+    }
     return 0;
 }
