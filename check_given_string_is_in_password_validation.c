bb4809a16f95f881f6cf7d3194a161733e4e8bda
MadhurMietID <146841949+MadhurMietID@users.noreply.github.com>
2024-01-07 11:28:27 +0530

    Up check_given_string_is_in_password_validation.c

--- check_given_string_is_in_password_validation.c
+++ check_given_string_is_in_password_validation.c
@@ -1,80 +1,78 @@
-// C program to check if a given string is a valid password.
-// The password must have a length of at least 8 characters,
-// contain at least one lowercase letter, one uppercase letter,one digit, and one special character.
-
+/*Program to check validity of a password.
+A valid password must have atleast 8 characters. One upper case leter , one lower case letter,
+one digit,one special character*/
 #include <stdio.h>
 #include <string.h>
-int main()
+int main() 
 {
     char str[100];
     int i, n, flag = 0;
     printf("Enter the string: ");
-    scanf("%s",str);
+    scanf("%s", str);
     n = strlen(str);
     // Check if the length of the password is at least 8 characters
-    if (n < 8)
+    if (n < 8) 
     {
         printf("Invalid password\n");
+        return 0;
     }
     // Check if the password contains at least one lowercase letter
-    for (i = 0; i < n; i++)
+    for (i = 0; i < n; i++) 
     {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
+        if (str[i] >= 'a' && str[i] <= 'z') {
             flag = 1;
             break;
         }
     }
-    if (flag == 0)
+    if (flag == 0) 
     {
         printf("Invalid password\n");
         return 0;
     }
     // Check if the password contains at least one uppercase letter
-    for (i = 0; i < n; i++)
+    flag = 0; // Reset flag for the next condition
+    for (i = 0; i < n; i++) 
     {
-        if (str[i] >= 'A' && str[i] <= 'Z')
-        {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
             flag = 1;
             break;
         }
     }
-    if (flag == 0)
-    {
+    if (flag == 0) {
         printf("Invalid password\n");
         return 0;
     }
     // Check if the password contains at least one digit
-    for (i = 0; i < n; i++)
-    {
-        if (str[i] >= '0' && str[i] <= '9')
+    flag = 0; // Reset flag for the next condition
+    for (i = 0; i < n; i++) {
+        if (str[i] >= '0' && str[i] <= '9') 
         {
             flag = 1;
             break;
         }
     }
-    if (flag == 0)
-    {
+    if (flag == 0) {
         printf("Invalid password\n");
         return 0;
     }
     // Check if the password contains at least one special character
-    for (i = 0; i < n; i++)
+    flag = 0; // Reset flag for the next condition
+    for (i = 0; i < n; i++) 
     {
-        if (str[i] == '!' || str[i] == '@' || str[i] == '#' || str[i] == '^' || str[i] == '&' || str[i] == '*' || str[i] == '(' || str[i] == ')' || str[i] == '-' || str[i] == '+' || str[i] == '=' || str[i] == '[' || str[i] == ']' || str[i] == '{' || str[i] == '}' || str[i] == ';' || str[i] == ':' || str[i] == ',' || str[i] == '.' || str[i] == '?' || str[i] == '/')
+        if (str[i] == '!' || str[i] == '@' || str[i] == '#' || str[i] == '^' || str[i] == '&' || str[i] == '*' || str[i] == '(' || str[i] == ')' || str[i] == '-' || str[i] == '+' || str[i] == '=' || str[i] == '[' || str[i] == ']' || str[i] == '{' || str[i] == '}' || str[i] == ';' || str[i] == ':' || str[i] == ',' || str[i] == '.' || str[i] == '?' || str[i] == '/') 
         {
             flag = 1;
             break;
         }
     }
-    if(flag==0)
+    if (flag == 0) 
     {
         printf("Invalid password\n");
-        return 0;   
-    }
-    else
+        return 0;
+    } 
+    else 
     {
-    printf("Valid password\n");
+        printf("Valid password\n");
     }
     return 0;
 }
